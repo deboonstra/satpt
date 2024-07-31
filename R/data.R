404060bed6d1851fd0e47937aade6523382b4175
deboonstra <daniel-boonstra@uiowa.edu>
2024-07-31 13:11:26 -0500

    Documenting ein data

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' @title Emerging Infectious Network (EIN) survey data
+#'
+#' @description Responses to a survey question about advanced molecular
+#' diagnostic testing for infectious diseases presented to medical personnel
+#' who are members of EIN.
+#'
+#' @format ## `ein`
+#' A \code{data.frame} with 643 observations and 2 columns.
+#' \describe{
+#'  \item{\code{responses}}{The responses to how often within the past two years
+#'  advanced molecular diagnostic testing was performed. Possible values are
+#'  *Not at all*, *Once*, *Rarely* (quarterly or less often), *Sometimes*
+#'  (monthly), and *Often* (weekly).}
+#'  \item{\code{wave}}{The data collect interval the responses were collected
+#'  during.}
+#' }
+#'
+#' @usage data(ein)
+"ein"
