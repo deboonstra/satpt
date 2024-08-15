b098fbf6c632ccd23c1e2924d0b0ac6ee660440c
deboonstra <daniel-boonstra@uiowa.edu>
2024-08-15 10:26:12 -0500

    Adding in details section to documentation and switching names to dimnames

--- R/satpt.R
+++ R/satpt.R
@@ -9,16 +9,50 @@
 #' @param exclude Vector of values that should be excluded in `y` and
 #' `by`. Generally, this should be used to denote missing values. Default
 #' is `NA` and `NaN`.
-#' @param alpha Significance level for Pearson's \eqn{\chi^2} for independence.
-#' Default is `0.05`.
-#' @param threshold Saturation threshold applied to the standard errors. Default
-#' is \code{0.05}.
-#' @param names Character vector of names for `y` and `by` when
+#' @param alpha Significance level for Pearson's \eqn{\chi^2} test for
+#' independence. Default is `0.05`.
+#' @param threshold Saturation threshold applied to the standard errors of the
+#' sample proportions. Default is \code{0.05}.
+#' @param dimnames Character vector of names for `y` and `by` when
 #' displaying the contingency table, sample proportions, and standard error
-#' matrices. Default is `NULL`. When `names` is specified the first entry should
-#' be name of `y` and the second entry should be name of `by`.
+#' matrices. When `dimnames` is an unnamed vector the first entry should be name
+#' of `y` variable and the second entry should be name of `by` variable. If
+#' `dimnames` is a named vector, then order of the values do NOT matter, as long
+#' the elements are named with `"y"` and `"by"`. Default is `NULL`.
 #'
-#' @details PLACE HOLDER
+#' @details The `by` argument should be specified when the responses collected
+#' in `y` are a by-product of the data collection mechanism defined by the `by`
+#' variable. When there is no a priori data collection mechanism defined, the
+#' `by` argument should not be defined. Generally, when the data is collected
+#' randomly or collected during the first data collection period, there is "no
+#' a priori data collection mechanism."
+#'
+#' Functionality of `satpt()` depends on the limited use of [stats::ftable()].
+#' More specifically, `y`, `by`, and `exclude` are directly used with
+#' [stats::ftable()] to create the contingency table of the collected data. The
+#' contingency table created by [stats::ftable()] is converted to a `matrix` for
+#' easier use with other functions. When the `dimnames` argument is used, it
+#' manipulates the attributes of the created `ftable`, which impacts the
+#' dimension names of the resulting `matrix`.
+#'
+#' Specification of the `by` argument automatically calls for a Pearson's
+#' \eqn{\chi^2} test for independence and the heterogeneity index of the sample
+#' proportions to be calculated. The \eqn{\chi^2} test for independence is
+#' conducted to determine if response bias is present within the responses in
+#' `y` are due to the data collection periods (`by`). When response bias is
+#' present the pooled standard errors of the overall sample proportions for each
+#' response category is reported, the pooled standard errors account for the
+#' response bias. The heterogeneity index is defined as the mean
+#' absoluted difference of the sample proportions for each response category
+#' within each data collection period (`by`) relative to the overall sample
+#' proportions for each response category. This index reflects the average
+#' deviation of the data collection period proportions from the overall sample
+#' proportions. Smaller values indicate the sample proportions of the data
+#' collection periods are less dissimilar. This measure is of importance when
+#' response bias is present. When `by` is not specified the test for
+#' independence will not be conducted and the heterogeneity index will not be
+#' calculated. `satpt` assumes response is only possible when `by` is specified.
+#' Thus, there is no need to check for response bias when `by` is not specified.
 #'
 #' @return An object with `S3` class `"satpt"` containing
 #' \describe{
@@ -34,7 +68,7 @@
 #'  \item{`se`}{A `matrix` containing the standard errors for the calculated
 #'  sample proportions (`phat`).}
 #'  \item{`test`}{A `htest` object produced by [stats::chisq.test()] containing
-#'  the results from the \eqn{\chi^2} for independence.}
+#'  the results from the \eqn{\chi^2} test for independence.}
 #'  \item{`total_obs`}{Total number of observations with a response provided.}
 #'  \item{`total`}{A `data.frame` with 5 variables describing the overall
 #'  collected sample. The `categories` variable provides the unique
@@ -69,7 +103,7 @@
 #' @export
 satpt <- function(
   y, by = NULL, exclude = c(NA, NaN), alpha = 0.05, threshold = 0.05,
-  names = NULL
+  dimnames = NULL
 ) {
 
   # Checking parameter types ####
@@ -95,29 +129,41 @@ satpt <- function(
     stop("threshold must be numeric.")
   }
 
-  if (!is.null(names)) {
-    if (!is.character(names)) {
-      stop("names must be a character vector.")
+  if (!is.null(dimnames)) {
+    if (!is.character(dimnames)) {
+      stop("dimnames must be a character vector.")
     }
-    if (length(names) == 1 && !is.null(by)) {
-      stop("names must be of length two when by is specified.")
+    if (length(dimnames) == 1 && !is.null(by)) {
+      stop("dimnames must be of length two when by is specified.")
+    }
+    if (!is.null(names(dimnames)) && !(c("y", "by") %in% names(dimnames))) {
+      stop("For a named dimnames vector, 'y' and 'by' must be included.")
     }
   }
 
   # Obtaining counts ####
   if (is.null(by)) {
     counts <- stats::ftable(y, exclude = exclude)
-    if (!is.null(names)) {
+    if (!is.null(dimnames)) {
       tmp <- attributes(counts)
-      names(tmp$col.vars) <- names[1]
+      if (is.null(names(dimnames))) {
+        names(tmp$col.vars) <- dimnames[1]
+      } else {
+        names(tmp$col.vars) <- unname(dimnames["y"])
+      }
       attributes(counts) <- tmp
     }
   } else {
     counts <- stats::ftable(y ~ by, exclude = exclude)
-    if (!is.null(names)) {
+    if (!is.null(dimnames)) {
       tmp <- attributes(counts)
-      names(tmp$col.vars) <- names[1]
-      names(tmp$row.vars) <- names[2]
+      if (is.null(names(dimnames))) {
+        names(tmp$col.vars) <- dimnames[1]
+        names(tmp$row.vars) <- dimnames[2]
+      } else {
+        names(tmp$col.vars) <- unname(dimnames["y"])
+        names(tmp$row.vars) <- unname(dimnames["by"])
+      }
       attributes(counts) <- tmp
     }
   }
