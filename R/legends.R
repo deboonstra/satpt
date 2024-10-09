e0bcbe825f4df99f95e7ee44f274ad8410a871d5
deboonstra <daniel-boonstra@uiowa.edu>
2024-10-09 11:41:28 -0500

    Creating legend and plotting functions

---
+++ R/legends.R
@@ -0,0 +1,85 @@
+#' @title Automatic placement of legends for plots
+#'
+#' @description Produce a legend either above or to the right of the plot
+#' window.
+#'
+#' @param horiz logical; if `TRUE`, set the legend horizontally rather than
+#' vertically (specifying `horiz` overrides the `ncol` specification)
+#' @param ... Further arguments to be passed to [graphics::legend()]
+#'
+#' @seealso [graphics::legend()]
+#'
+#' @examples
+#' # loading data
+#' data(ein)
+#'
+#' # performing saturation point analysis
+#' fit <- satpt::satpt(
+#'   y = ein$responses,
+#'   by = ein$wave,
+#'   dimnames = c("Response collected", "Collection period")
+#' )
+#'
+#' # plotting standard errors
+#' graphics::par(oma = c(0, 0, 0, 8))
+#' plot(fit, threshold = TRUE)
+#'
+#' # adding legend
+#' legend_right(
+#'   legend = "Saturation\nthreshold",
+#'   col = "firebrick", lty = 3, lwd = 2
+#' )
+#'
+#' @rdname legends
+#' @export legend_top
+legend_top <- function(horiz = TRUE, ...) {
+  if (graphics::par("oma")[3] == 0) {
+    x <- mean(graphics::par("usr")[1:2])
+    yy <- transform_coord(graphics::par("usr")[3:4], graphics::par("plt")[3:4])
+    y  <- mean(c(yy[2], graphics::par("usr")[4]))
+    graphics::legend(
+      x, y, xpd = NA, bty = "n", xjust = 0.5, yjust = 0.5, horiz = horiz, ...
+    )
+  } else {
+    g <- graphics::par("mfrow")
+    xx <- transform_coord(graphics::par("usr")[1:2], graphics::par("plt")[1:2])
+    yy <- transform_coord(graphics::par("usr")[3:4], graphics::par("plt")[3:4])
+    xxx <- transform_coord(xx, c(g[2] - 1, g[2]) / g[2])
+    yyy <- transform_coord(yy, c(g[1] - 1, g[1]) / g[1])
+    yyyy <- transform_coord(yyy, graphics::par("omd")[3:4])
+    graphics::legend(
+      mean(xxx), mean(c(yyy[2], yyyy[2])), xpd = NA, bty = "n",
+      xjust = 0.5, yjust = 0.5, horiz = horiz, ...
+    )
+  }
+}
+
+#' @rdname legends
+#' @export legend_right
+legend_right <- function(...) {
+  if (graphics::par("oma")[4] == 0) {
+    y <- mean(graphics::par("usr")[3:4])
+    xx <- transform_coord(graphics::par("usr")[1:2], graphics::par("plt")[1:2])
+    x <- mean(c(xx[2], graphics::par("usr")[2]))
+    graphics::legend(x, y, xpd = NA, bty = "n", xjust = 0.5, yjust = 0.5, ...)
+  } else {
+    g <- graphics::par("mfrow")
+    xx <- transform_coord(graphics::par("usr")[1:2], graphics::par("plt")[1:2])
+    yy <- transform_coord(graphics::par("usr")[3:4], graphics::par("plt")[3:4])
+    xxx <- transform_coord(xx, c(g[2] - 1, g[2]) / g[2])
+    yyy <- transform_coord(yy, c(g[1] - 1, g[1]) / g[1])
+    xxxx <- transform_coord(xxx, graphics::par("omd")[1:2])
+    graphics::legend(
+      mean(c(xxx[2], xxxx[2])), mean(yyy), xpd = NA, bty = "n",
+      xjust = 0.5, yjust = 0.5, ...
+    )
+  }
+}
+
+# Helper function ####
+transform_coord <- function(x, p) {
+  ba <- (x[2] - x[1]) / (p[2] - p[1])
+  a <- x[1] - p[1] * ba
+  b <- a + ba
+  c(a, b)
+}
---
+++ R/plot.R
@@ -0,0 +1,72 @@
+#' @title Plot of saturation point analysis
+#'
+#' @description Plotting of the overall standard errors calculated during the
+#' saturation point anlaysis performed by [satpt::satpt()].
+#'
+#' @param x `satpt` object to be plotted.
+#' @param threshold logical; if `TRUE`, display saturation threshold.
+#' Default is `TRUE`.
+#' @param \dots Further arguments to be passed to [graphics::barplot()]
+#'
+#' @seealso [graphics::barplot()]
+#'
+#' @examples
+#' # loading data
+#' data(ein)
+#'
+#' # performing saturation point analysis
+#' fit1 <- satpt::satpt(y = ein$responses)
+#' fit2 <- satpt::satpt(
+#'   y = ein$responses,
+#'   by = ein$wave,
+#'   dimnames = c("Response collected", "Collection period")
+#' )
+#'
+#' # plotting standard errors
+#' plot(fit1)
+#' plot(fit2)
+#'
+#' @export
+plot.satpt <- function(x, threshold = TRUE, ...) {
+  # Check object type ####
+  if (!methods::is(x, "satpt")) {
+    stop("x must be of satpt type.")
+  }
+
+  # Specifying default plotting parameters ####
+
+  ## Determining limit of y-axis ####
+  se_max <- signif(
+    abs(signif(max(x$total$se), digits = 1) - max(x$total$se)) +
+      signif(max(x$total$se), digits = 1),
+    digits = 1
+  )
+  ylim_max <- max(x$threshold, se_max)
+
+  ## Sepcifying plotting parameter ####
+  barplot_args <- list(
+    height = x$total$se,
+    names = x$total$categories,
+    xlab = names(dimnames(x$phat))[2],
+    ylab = "Standard errors",
+    ylim = c(0, ylim_max),
+    main = "Standard errors of overall sample proportions",
+    border = NA
+  )
+
+  # Pulling additional plotting parameters from user ####
+  new_args <- list(...)
+
+  ## Updating default plotting parameters ####
+  if (length(new_args)) {
+    barplot_args[names(new_args)] <- new_args
+  }
+
+  # Plotting standard errors ####
+  do.call(what = "barplot", args = barplot_args)
+
+  ## Adding horizontal line for saturationa threshold ####
+  if (threshold) {
+    graphics::abline(h = x$threshold, col = "firebrick", lty = 3, lwd = 2)
+  }
+}
